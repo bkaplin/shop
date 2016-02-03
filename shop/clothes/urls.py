632609987b27aaba3fd8202f5992ff130df33b55
Borya Kaplin <bobsley345@mail.ru>
2016-02-03 14:24:28 +0200

    Add detail

--- shop/clothes/urls.py
+++ shop/clothes/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from clothes.views import all_clothes
+from clothes.views import all_clothes, detail
 
 urlpatterns = patterns('',
     url(r'^$', all_clothes, name='all_clothes'),
+	url(r'^(?P<pk>\d+)/$', detail, name='detail'),
 )
